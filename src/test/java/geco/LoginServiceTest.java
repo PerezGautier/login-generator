75b068043aa995a807b8f85ce976d9f52a585461
PerezGautier <gautier.perez@etud.univ-jfc.fr>
2020-11-16 11:51:05 +0100

    close #1 add all junit test to LoginServiceTest

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -2,17 +2,43 @@ package geco;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+
 public class LoginServiceTest extends TestCase {
 
+    private LoginService loginService;
+    private String login;
+    private String login2;
+
+    @Override
+    public void setUp() {
+        String[] loginsExistants={"123456789","password"};
+        loginService = new LoginService(loginsExistants);
+        login = "123456789";
+        login2 = "helloworld";
+
+    }
+
     public void testLoginExists() {
+        assertTrue(loginService.loginExists(login));
+        assertFalse(loginService.loginExists(login2));
     }
 
     public void testAddLogin() {
+        assertFalse(loginService.loginExists("aaaaaaaaa"));
+        loginService.addLogin("aaaaaaaaa");
+        assertTrue(loginService.loginExists("aaaaaaaaa"));
     }
 
     public void testFindAllLoginsStartingWith() {
+        List<String> allLogin = loginService.findAllLoginsStartingWith("1");
+        assertEquals(1, allLogin.size());
+        assertEquals("123456789", allLogin.get(0));
     }
 
     public void testFindAllLogins() {
+        List<String> allLogin = loginService.findAllLogins();
+        assertEquals(2, allLogin.size());
     }
+
 }
